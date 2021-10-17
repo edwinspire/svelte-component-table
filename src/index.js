1ae5ebc522fb77093ade01419550a3b71516556e
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-10-17 06:43:13 -0500

    Cambio en la tabla para que permita ingresar como parámetro el decorador de la columna 4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Table from "Table/Table.svelte";
-const types = require("Table/Column/DefaultTypes.js");
+const types = require("./Table/Column/DefaultTypes");
 //export { default as default } from "./Table/Table.svelte";
 module.exports = {
   Table: Table,

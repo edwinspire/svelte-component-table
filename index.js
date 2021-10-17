0615f14bff7253b88879af475335f778235a6ba1
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-10-17 06:50:57 -0500

    Cambio en la tabla para que permita ingresar como parámetro el decorador de la columna 5

---
+++ index.js
@@ -0,0 +1,7 @@
+import Table from "Table/Table.svelte";
+const types = require("./Table/Column/DefaultTypes");
+//export { default as default } from "./Table/Table.svelte";
+module.exports = {
+  Table: Table,
+  ColumnTypes: types,
+};

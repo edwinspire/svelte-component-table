ac8bb961aea10d1be3dd95dfd32d9bef7fd51184
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz>
2021-12-01 09:26:19 -0500

    Correccion de bug en columna tipo boolean

--- src/Table/Column/Boolean.svelte
+++ src/Table/Column/Boolean.svelte
@@ -3,42 +3,46 @@
   export const row = {};
   export let props = {};
   let option = "default";
+  let config = {
+    icon: {
+      true: {
+        td_class: "has-text-centered",
+        class: "icon has-text-success",
+        icon: "fas fa-check",
+      },
+      false: {
+        td_class: "has-text-centered",
+        class: "icon has-text-danger",
+        icon: "fas fa-times",
+      },
+    },
+    "color-icon": {
+      true: {
+        td_class: "has-background-success has-text-white has-text-centered",
+        class: "icon",
+        icon: "fas fa-check",
+      },
+      false: {
+        td_class: "has-background-danger has-text-white has-text-centered",
+        class: "icon",
+        icon: "fas fa-times",
+      },
+    },
+  };
 
   if (props && props.option) {
-    option = props.option;
+    option = config[props.option];
   }
-  //console.log(props);
+//  console.log(option, value);
 </script>
 
-{#if value}
-  {#if option == "icon"}
-    <td class="has-text-centered" on:click>
-      <div class="icon-text">
-        <span class="icon has-text-success">
-          <i class="fas fa-check" />
-        </span>
-      </div>
-    </td>
-  {:else if option == "color-icon"}
-    <td on:click class="has-background-danger has-text-white has-text-centered">
-      <i class="fa fa-check" aria-hidden="true" />
-    </td>
-  {:else}
-    <td class="has-text-centered" on:click>
-      <input type="checkbox" checked={value} onclick="return false;" />
-    </td>
-  {/if}
-{:else if option == "icon"}
-  <td class="has-text-centered" on:click>
+{#if option}
+  <td class={option[value].td_class} on:click>
     <div class="icon-text">
-      <span class="icon has-text-danger">
-        <i class="fas fa-times" />
+      <span class={option[value].class}>
+        <i class={option[value].icon} />
       </span>
     </div>
-  </td>
-{:else if option == "color-icon"}
-  <td on:click class="has-background-danger has-text-white has-text-centered">
-    <i class="fa fa-times" aria-hidden="true" />
   </td>
 {:else}
   <td class="has-text-centered" on:click>
